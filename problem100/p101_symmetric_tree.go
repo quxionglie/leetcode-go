2e6beb4ae1fa80fb3eee14184f6bad60f4ba4068
xionglieq <xionglie.qu@gmail.com>
2022-04-30 21:19:16 +0800

    101. 对称二叉树

---
+++ problem100/p101_symmetric_tree.go
@@ -0,0 +1,44 @@
+package problem100
+
+/**
+101. 对称二叉树
+给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+*/
+func isSymmetric(root *TreeNode) bool {
+	//递归解决: 引入一个队列，这是把递归程序改写成迭代程序的常用方法。
+	//初始化时我们把根节点入队两次。每次提取两个结点并比较它们的值（队列中每两个连续的结点应该是相等的，而且它们的子树互为镜像），
+	//然后将两个结点的左右子结点按相反的顺序插入队列中。当队列为空时，
+	//或者我们检测到树不对称（即从队列中取出两个不相等的连续结点）时，该算法结束。
+	queue := []*TreeNode{}
+	queue = append(queue, root)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		curLeft := queue[0]
+		curRight := queue[1]
+		queue = queue[2:]
+
+		if curLeft == nil && curRight == nil {
+			continue
+		}
+		if curLeft == nil || curRight == nil {
+			return false
+		}
+		if curLeft.Val != curRight.Val {
+			return false
+		}
+
+		queue = append(queue, curLeft.Left)
+		queue = append(queue, curRight.Right)
+
+		queue = append(queue, curLeft.Right)
+		queue = append(queue, curRight.Left)
+	}
+
+	return true
+}
---
+++ problem100/p101_symmetric_tree_test.go
@@ -0,0 +1,69 @@
+package problem100
+
+import "testing"
+
+/**
+    1
+  2    2
+3  4  4  3
+*/
+func Test_isSymmetric(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"", args{
+			root: &TreeNode{Val: 1,
+				Left: &TreeNode{Val: 2,
+					Left: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val:   4,
+						Left:  nil,
+						Right: nil}},
+				Right: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val:   4,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil},
+				}}}, true}, //[1,2,2,3,4,4,3] true
+		{"", args{
+			root: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{Val: 2,
+					Left: nil,
+					Right: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil}},
+				Right: &TreeNode{
+					Val:  2,
+					Left: nil,
+					Right: &TreeNode{
+						Val:   3,
+						Left:  nil,
+						Right: nil},
+				}}}, false}, //[1,2,2,null,3,null,3] false
+		{"", args{&TreeNode{}}, true}, //[1,2,2,null,3,null,3] false
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSymmetric(tt.args.root); got != tt.want {
+				t.Errorf("isSymmetric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
