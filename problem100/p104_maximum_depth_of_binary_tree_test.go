72b2a6622fb1edfba9a131266bb6fb9cfde3991c
xionglieq <xionglie.qu@gmail.com>
2022-05-01 11:06:24 +0800

    104. 二叉树的最大深度

---
+++ problem100/p104_maximum_depth_of_binary_tree_test.go
@@ -0,0 +1,51 @@
+package problem100
+
+import "testing"
+
+/**
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+retru 3
+*/
+func Test_maxDepth(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"", args{
+			root: &TreeNode{Val: 3,
+				Left: &TreeNode{
+					Val:   9,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val: 20,
+					Left: &TreeNode{
+						Val:   15,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val:   7,
+						Left:  nil,
+						Right: nil},
+				}}}, 3},
+		{"", args{root: nil}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.args.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
