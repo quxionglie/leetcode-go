3d1bb0f74c9a30c3ed45b58e7a6a4fdc69a85ae9
xionglieq <xionglie.qu@gmail.com>
2022-05-22 23:10:32 +0800

    160. 相交链表

---
+++ problem100/p160_intersection_of_two_linked_lists.go
@@ -0,0 +1,33 @@
+package problem100
+
+/**
+160. 相交链表
+给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+图示两个链表在节点 c1 开始相交：
+题目数据 保证 整个链式结构中不存在环。
+注意，函数返回结果后，链表必须 保持其原始结构 。
+a1->a2->a3->a4
+b1->b2->a3->a4
+return a3
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+*/
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	aMap := make(map[*ListNode]bool)
+
+	pa, pb := headA, headB
+	for pa != nil {
+		aMap[pa] = true
+		pa = pa.Next
+	}
+	for pb != nil {
+		if _, ok := aMap[pb]; ok {
+			return pb
+		}
+		pb = pb.Next
+	}
+	return nil
+}
