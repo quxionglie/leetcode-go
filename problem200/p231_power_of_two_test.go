d4204d94ec363082cc412e325db177fe4a55bcc7
xionglieq <xionglie.qu@gmail.com>
2022-06-03 21:36:29 +0800

    231. 2 的幂

---
+++ problem200/p231_power_of_two_test.go
@@ -0,0 +1,37 @@
+package problem200
+
+import "testing"
+
+func Test_isPowerOfTwo(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{1},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{3},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{16},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPowerOfTwo(tt.args.n); got != tt.want {
+				t.Errorf("isPowerOfTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
