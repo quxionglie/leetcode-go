30ea5cbf2a5bd5411fc56b5b3195269d9497d23d
xionglieq <xionglie.qu@gmail.com>
2022-06-14 23:21:51 +0800

    342. 4的幂

---
+++ problem300/p342_power_of_four_test.go
@@ -0,0 +1,37 @@
+package problem300
+
+import "testing"
+
+func Test_isPowerOfFour(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{16},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{5},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{1},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPowerOfFour(tt.args.n); got != tt.want {
+				t.Errorf("isPowerOfFour() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
