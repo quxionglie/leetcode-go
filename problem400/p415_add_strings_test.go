a09215a7ca0bf48943dd70fedcd85c3ab0a1e0e3
xionglieq <xionglie.qu@gmail.com>
2022-06-26 08:26:13 +0800

    415. 字符串相加

---
+++ problem400/p415_add_strings_test.go
@@ -0,0 +1,33 @@
+package problem400
+
+import "testing"
+
+func Test_addStrings(t *testing.T) {
+	type args struct {
+		num1 string
+		num2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{"456", "77"},
+			want: "533",
+		},
+		{
+			name: "",
+			args: args{"0", "0"},
+			want: "0",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addStrings(tt.args.num1, tt.args.num2); got != tt.want {
+				t.Errorf("addStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
