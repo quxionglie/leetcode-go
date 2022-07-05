165b4f88464c23c232190d68b9d03f670b6ad3e1
xionglieq <xionglie.qu@gmail.com>
2022-07-05 22:14:52 +0800

    463. 岛屿的周长

---
+++ problem400/p463_island_perimeter_test.go
@@ -0,0 +1,37 @@
+package problem400
+
+import "testing"
+
+func Test_islandPerimeter(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{[][]int{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}},
+			want: 16,
+		},
+		{
+			name: "",
+			args: args{[][]int{{1}}},
+			want: 4,
+		},
+		{
+			name: "",
+			args: args{[][]int{{1, 0}}},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := islandPerimeter(tt.args.grid); got != tt.want {
+				t.Errorf("islandPerimeter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
