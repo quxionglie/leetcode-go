b5099dd8d53ce633402436c6f3e187409761cafd
xionglieq <xionglie.qu@gmail.com>
2022-07-23 19:33:35 +0800

    530. 二叉搜索树的最小绝对差

---
+++ problem500/p530_minimum_absolute_difference_in_bst_test.go
@@ -0,0 +1,49 @@
+package problem500
+
+import "testing"
+
+func Test_getMinimumDifference(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				root: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val:   1,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val:   3,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+					Right: &TreeNode{
+						Val:   6,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getMinimumDifference(tt.args.root); got != tt.want {
+				t.Errorf("getMinimumDifference() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
