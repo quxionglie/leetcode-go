51be054333667c6a48f172dd0ddfd9284f9de0b6
xionglieq <xionglie.qu@gmail.com>
2022-07-27 21:38:53 +0800

    563. 二叉树的坡度

---
+++ problem500/p563_binary_tree_tilt.go
@@ -0,0 +1,48 @@
+package problem500
+
+/**
+563. 二叉树的坡度
+给你一个二叉树的根节点 root ，计算并返回 整个树 的坡度 。
+一个树的 节点的坡度 定义即为，该节点左子树的节点之和和右子树节点之和的 差的绝对值 。
+如果没有左子树的话，左子树的节点之和为 0 ；没有右子树的话也是一样。空结点的坡度是 0 。
+整个树 的坡度就是其所有节点的坡度之和。
+
+输入：root = [1,2,3]
+输出：1
+解释：
+节点 2 的坡度：|0-0| = 0（没有子节点）
+节点 3 的坡度：|0-0| = 0（没有子节点）
+节点 1 的坡度：|2-3| = 1（左子树就是左子节点，所以和是 2 ；右子树就是右子节点，所以和是 3 ）
+坡度总和：0 + 0 + 1 = 1
+
+ 提示：
+树中节点数目的范围在 [0, 104] 内
+-1000 <= Node.val <= 1000
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+*/
+func findTilt(root *TreeNode) (ans int) {
+	var dfs func(*TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		sumLeft := dfs(node.Left)
+		sumRight := dfs(node.Right)
+		ans += abs(sumLeft - sumRight)
+		return sumLeft + sumRight + node.Val
+	}
+	dfs(root)
+	return
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
