678dd8b07be81620afd08b3dce9f9a5cfacf6633
xionglieq <xionglie.qu@gmail.com>
2022-08-15 22:43:58 +0800

    643. 子数组最大平均数 I

---
+++ problem600/p643_maximum_average_subarray_i_test.go
@@ -0,0 +1,31 @@
+package problem600
+
+import "testing"
+
+func Test_findMaxAverage(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 12, -5, -6, 50, 3},
+				k:    4,
+			},
+			want: 12.75,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMaxAverage(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("findMaxAverage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
