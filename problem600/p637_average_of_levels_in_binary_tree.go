ad6d0598b29c127c41a0074d552a00e2d8dbcae9
xionglieq <xionglie.qu@gmail.com>
2022-08-20 23:33:52 +0800

    637. 二叉树的层平均值

---
+++ problem600/p637_average_of_levels_in_binary_tree.go
@@ -0,0 +1,46 @@
+package problem600
+
+/**
+637. 二叉树的层平均值
+给定一个非空二叉树的根节点 root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10-5 以内的答案可以被接受。
+示例 1：
+输入：root = [3,9,20,null,null,15,7]
+输出：[3.00000,14.50000,11.00000]
+解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+因此返回 [3, 14.5, 11]
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+*/
+
+func averageOfLevels(root *TreeNode) []float64 {
+	type data struct {
+		sum   int
+		count int
+	}
+	levelData := []data{}
+	var dfs func(node *TreeNode, level int)
+	dfs = func(node *TreeNode, level int) {
+		if node == nil {
+			return
+		}
+		if level < len(levelData) {
+			levelData[level].sum += node.Val
+			levelData[level].count++
+		} else {
+			levelData = append(levelData, data{node.Val, 1})
+		}
+		dfs(node.Left, level+1)
+		dfs(node.Right, level+1)
+	}
+	dfs(root, 0)
+
+	averages := make([]float64, len(levelData))
+	for i, d := range levelData {
+		averages[i] = float64(d.sum) / float64(d.count)
+	}
+	return averages
+}
