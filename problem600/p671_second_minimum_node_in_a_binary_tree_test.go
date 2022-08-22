97d6cd2bfe17df0c16d0d87924d9dbcbf306703e
xionglieq <xionglie.qu@gmail.com>
2022-08-22 23:03:39 +0800

    671. 二叉树中第二小的节点

---
+++ problem600/p671_second_minimum_node_in_a_binary_tree_test.go
@@ -0,0 +1,49 @@
+package problem600
+
+import "testing"
+
+func Test_findSecondMinimumValue(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				root: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val:   2,
+						Left:  nil,
+						Right: nil,
+					},
+					Right: &TreeNode{
+						Val: 5,
+						Left: &TreeNode{
+							Val:   5,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val:   7,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+				},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findSecondMinimumValue(tt.args.root); got != tt.want {
+				t.Errorf("findSecondMinimumValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
