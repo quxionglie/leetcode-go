926e028639d9c5e35e65b374473a5242c9843b3d
xionglieq <xionglie.qu@gmail.com>
2022-08-23 22:30:08 +0800

    680. 验证回文字符串 Ⅱ

---
+++ problem600/p680_valid_palindrome_ii _test.go
@@ -0,0 +1,43 @@
+package problem600
+
+import "testing"
+
+func Test_validPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{
+				s: "aba",
+			},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{
+				s: "abca",
+			},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{
+				s: "abc",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("validPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
