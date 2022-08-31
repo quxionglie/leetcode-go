a8687aed48d88976d325210c162879b265855284
xionglieq <xionglie.qu@gmail.com>
2022-08-31 22:29:14 +0800

    796. 旋转字符串

---
+++ problem700/p796_rotate_string_test.go
@@ -0,0 +1,39 @@
+package problem700
+
+import "testing"
+
+func Test_rotateString(t *testing.T) {
+	type args struct {
+		s    string
+		goal string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{
+				s:    "abcde",
+				goal: "cdeab",
+			},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{
+				s:    "abcde",
+				goal: "abced",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rotateString(tt.args.s, tt.args.goal); got != tt.want {
+				t.Errorf("rotateString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
