9a1b8e812e4d7c547c6bb278f266a3ce5c33d3f5
xionglieq <xionglie.qu@gmail.com>
2022-09-04 22:22:21 +0800

    3. 无重复字符的最长子串

---
+++ problem000/p003_longest_substring_without_repeating_characters_test.go
@@ -0,0 +1,37 @@
+package problem000
+
+import "testing"
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{"abcabcbb"},
+			want: 3,
+		},
+		{
+			name: "",
+			args: args{"bbbbb"},
+			want: 1,
+		},
+		{
+			name: "",
+			args: args{"pwwkew"},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
