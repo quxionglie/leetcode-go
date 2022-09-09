9d498bb1b20277b552ce04ac783416940a205f7b
xionglieq <xionglie.qu@gmail.com>
2022-09-09 19:50:32 +0800

    11. 盛最多水的容器

--- problem000/p011_container_with_most_water_test.go
+++ problem000/p011_container_with_most_water_test.go
@@ -16,6 +16,16 @@ func Test_maxArea(t *testing.T) {
 			args: args{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}},
 			want: 49,
 		},
+		{
+			name: "",
+			args: args{[]int{1, 1}},
+			want: 1,
+		},
+		{
+			name: "",
+			args: args{[]int{1, 2, 4, 6}},
+			want: 4,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
