7c997f844966c04151e2c1233442f69fb0406a7a
xionglieq <xionglie.qu@gmail.com>
2022-09-15 22:42:49 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ problem000/p019_remove_nth_node_from_end_of_list.go
@@ -0,0 +1,42 @@
+package problem000
+
+/*
+*
+19. 删除链表的倒数第 N 个结点
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+示例 1：
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+
+示例 2：
+输入：head = [1], n = 1
+输出：[]
+
+示例 3：
+输入：head = [1,2], n = 1
+输出：[1]
+提示：
+链表中结点的数目为 sz
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+  - Definition for singly-linked list.
+  - type ListNode struct {
+  - Val int
+  - Next *ListNode
+  - }
+*/
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	//栈
+	//遍历链表的同时将所有节点依次入栈。
+	//根据栈「先进后出」的原则，我们弹出栈的第 n 个节点就是需要删除的节点，
+	//并且目前栈顶的节点就是待删除节点的前驱节点。这样一来，删除操作就变得十分方便了。
+	nodes := []*ListNode{}
+	dummy := &ListNode{0, head}
+	for node := dummy; node != nil; node = node.Next {
+		nodes = append(nodes, node)
+	}
+	prev := nodes[len(nodes)-1-n]
+	prev.Next = prev.Next.Next
+	return dummy.Next
+}
---
+++ problem000/p019_remove_nth_node_from_end_of_list_test.go
@@ -0,0 +1,50 @@
+package problem000
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_removeNthFromEnd(t *testing.T) {
+	type args struct {
+		head *ListNode
+		n    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "",
+			args: args{
+				head: createLinkList([]int{1, 2, 3, 4, 5}),
+				n:    2,
+			},
+			want: createLinkList([]int{1, 2, 3, 5}),
+		},
+		{
+			name: "",
+			args: args{
+				head: createLinkList([]int{1}),
+				n:    1,
+			},
+			want: createLinkList([]int{}),
+		},
+		{
+			name: "",
+			args: args{
+				head: createLinkList([]int{1, 2}),
+				n:    1,
+			},
+			want: createLinkList([]int{1}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeNthFromEnd(tt.args.head, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeNthFromEnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
