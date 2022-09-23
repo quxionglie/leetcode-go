35d510b128b1209edba22e751625b7e707149c1e
xionglieq <xionglie.qu@gmail.com>
2022-09-23 09:19:11 +0800

    40. 组合总和 II

---
+++ problem000/p040_combination_sum2_test.go
@@ -0,0 +1,69 @@
+package problem000
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_combinationSum2(t *testing.T) {
+	type args struct {
+		candidates []int
+		target     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "",
+			args: args{
+				candidates: []int{10, 1, 2, 7, 6, 1, 5},
+				target:     8,
+			},
+			want: [][]int{
+				{1, 1, 6},
+				{1, 2, 5},
+				{1, 7},
+				{2, 6},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				candidates: []int{2, 5, 2, 1, 2},
+				target:     5,
+			},
+			want: [][]int{
+				{1, 2, 2},
+				{5},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				candidates: []int{2},
+				target:     1,
+			},
+			want: [][]int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := combinationSum2(tt.args.candidates, tt.args.target)
+			var eq bool
+			for _, g := range got {
+				for _, w := range tt.want {
+					if reflect.DeepEqual(g, w) {
+						eq = true
+						break
+					}
+				}
+				if !eq {
+					t.Errorf("combinationSum() = %v, want %v", got, tt.want)
+				}
+			}
+
+		})
+	}
+}
