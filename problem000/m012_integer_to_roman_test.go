c2247e6a6a16da2a41c0842caac8880466a514dc
xionglieq <xionglie.qu@gmail.com>
2022-10-05 15:44:47 +0800

    12.整数转罗马数字

---
+++ problem000/m012_integer_to_roman_test.go
@@ -0,0 +1,32 @@
+package problem000
+
+import "testing"
+
+func Test_intToRoman(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{3},
+			want: "III",
+		},
+		{
+			name: "",
+			args: args{1994},
+			want: "MCMXCIV",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intToRoman(tt.args.num); got != tt.want {
+				t.Errorf("intToRoman() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
