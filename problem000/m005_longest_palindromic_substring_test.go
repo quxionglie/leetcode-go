575cc2ca812d991f4e9ec833f211cb356c2d2860
xionglieq <xionglie.qu@gmail.com>
2022-10-06 17:15:08 +0800

    5. 最长回文子串

---
+++ problem000/m005_longest_palindromic_substring_test.go
@@ -0,0 +1,37 @@
+package problem000
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{"babad"},
+			want: "bab",
+		},
+		{
+			name: "",
+			args: args{"cbbd"},
+			want: "bb",
+		},
+		{
+			name: "",
+			args: args{"a"},
+			want: "a",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
