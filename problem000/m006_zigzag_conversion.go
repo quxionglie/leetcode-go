36f172f733e273e99e55b707cd4f548961b07e7c
xionglieq <xionglie.qu@gmail.com>
2022-10-07 22:46:15 +0800

    6. Z 字形变换

---
+++ problem000/m006_zigzag_conversion.go
@@ -0,0 +1,68 @@
+package problem000
+
+/*
+6. Z 字形变换
+将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+P   A   H   N
+A P L S I I G
+Y   I   R
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+
+请你实现这个将字符串进行指定行数变换的函数：
+string convert(string s, int numRows);
+
+示例 1：
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+
+示例 2：
+输入：s = "PAYPALISHIRING", numRows = 4
+输出："PINALSIGYAHRPI"
+解释：
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+示例 3：
+输入：s = "A", numRows = 1
+输出："A"
+
+提示：
+1 <= s.length <= 1000
+s 由英文字母（小写和大写）、',' 和 '.' 组成
+1 <= numRows <= 1000
+*/
+func convert(s string, numRows int) string {
+	//利用二维矩阵模拟
+	n, r := len(s), numRows
+	if r == 1 || r >= n {
+		return s
+	}
+	t := r*2 - 2
+	c := (n + t - 1) / t * (r - 1)
+	mat := make([][]byte, r)
+	for i := range mat {
+		mat[i] = make([]byte, c)
+	}
+	x, y := 0, 0
+	for i, ch := range s {
+		mat[x][y] = byte(ch)
+		if i%t < r-1 {
+			x++ // 向下移动
+		} else {
+			x--
+			y++ // 向右上移动
+		}
+	}
+	ans := make([]byte, 0, n)
+	for _, row := range mat {
+		for _, ch := range row {
+			if ch > 0 {
+				ans = append(ans, ch)
+			}
+		}
+	}
+	return string(ans)
+}
---
+++ problem000/m006_zigzag_conversion_test.go
@@ -0,0 +1,47 @@
+package problem000
+
+import "testing"
+
+func Test_convert(t *testing.T) {
+	type args struct {
+		s       string
+		numRows int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{
+				s:       "PAYPALISHIRING",
+				numRows: 3,
+			},
+			want: "PAHNAPLSIIGYIR",
+		},
+		{
+			name: "",
+			args: args{
+				s:       "PAYPALISHIRING",
+				numRows: 4,
+			},
+			want: "PINALSIGYAHRPI",
+		},
+		{
+			name: "",
+			args: args{
+				s:       "A",
+				numRows: 1,
+			},
+			want: "A",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convert(tt.args.s, tt.args.numRows); got != tt.want {
+				t.Errorf("convert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
