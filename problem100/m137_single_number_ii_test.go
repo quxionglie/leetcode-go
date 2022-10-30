e042fd71542200c90977bde102e63c031a6219ba
xionglieq <xionglie.qu@gmail.com>
2022-10-30 17:46:54 +0800

    137. 只出现一次的数字 II

---
+++ problem100/m137_single_number_ii_test.go
@@ -0,0 +1,29 @@
+package problem100
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				nums: []int{2, 2, 3, 2},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
