7bfe4c50264a04b31b5f5496ec074e4e6e28f5d2
xionglieq <xionglie.qu@gmail.com>
2022-11-23 23:13:49 +0800

    215. 数组中的第K个最大元素

--- problem200/m215_kth_largest_element_in_an_array.go
+++ problem200/m215_kth_largest_element_in_an_array.go
@@ -1,5 +1,10 @@
 package problem200
 
+import (
+	"math/rand"
+	"time"
+)
+
 /*
 *
 215. 数组中的第K个最大元素
@@ -19,32 +24,36 @@ package problem200
 -10^4 <= nums[i] <= 10^4
 */
 func findKthLargest(nums []int, k int) int {
-	heapSize := len(nums)
-	buildMaxHeap(nums, heapSize)
-	for i := len(nums) - 1; i >= len(nums)-k+1; i-- {
-		nums[0], nums[i] = nums[i], nums[0]
-		heapSize--
-		maxHeapify(nums, 0, heapSize)
-	}
-	return nums[0]
+	//方法一：基于快速排序的选择方法
+	rand.Seed(time.Now().UnixNano())
+	return quickSelect(nums, 0, len(nums)-1, len(nums)-k)
 }
 
-func buildMaxHeap(a []int, heapSize int) {
-	for i := heapSize / 2; i >= 0; i-- {
-		maxHeapify(a, i, heapSize)
+func quickSelect(a []int, l, r, index int) int {
+	q := randomPartition(a, l, r)
+	if q == index {
+		return a[q]
+	} else if q < index {
+		return quickSelect(a, q+1, r, index)
 	}
+	return quickSelect(a, l, q-1, index)
 }
 
-func maxHeapify(a []int, i, heapSize int) {
-	l, r, largest := i*2+1, i*2+2, i
-	if l < heapSize && a[l] > a[largest] {
-		largest = l
+func randomPartition(a []int, l, r int) int {
+	i := rand.Int()%(r-l+1) + l
+	a[i], a[r] = a[r], a[i]
+	return partition(a, l, r)
+}
+
+func partition(a []int, l, r int) int {
+	x := a[r]
+	i := l - 1
+	for j := l; j < r; j++ {
+		if a[j] <= x {
+			i++
+			a[i], a[j] = a[j], a[i]
+		}
 	}
-	if r < heapSize && a[r] > a[largest] {
-		largest = r
-	}
-	if largest != i {
-		a[i], a[largest] = a[largest], a[i]
-		maxHeapify(a, largest, heapSize)
-	}
+	a[i+1], a[r] = a[r], a[i+1]
+	return i + 1
 }
