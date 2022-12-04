ba2355cf65de180f19379e18f0faa808a90c4e19
xionglieq <xionglie.qu@gmail.com>
2022-12-04 12:31:15 +0800

    222. 完全二叉树的节点个数

---
+++ problem200/m222_count_complete_tree_nodes_test.go
@@ -0,0 +1,71 @@
+package problem200
+
+import "testing"
+
+func Test_countNodes(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				root: &TreeNode{},
+			},
+			want: 0,
+		},
+		{
+			name: "",
+			args: args{
+				root: &TreeNode{
+					Val:   0,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val:   4,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: &TreeNode{
+							Val:   5,
+							Left:  nil,
+							Right: nil,
+						},
+					},
+					Right: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val:   6,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: nil,
+					},
+				},
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countNodes(tt.args.root); got != tt.want {
+				t.Errorf("countNodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
