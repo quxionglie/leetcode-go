6d45277fdcf1ae0c46cc1431872121f12122d94e
xionglieq <xionglie.qu@gmail.com>
2022-12-16 21:14:50 +0800

    63. 不同路径 II

---
+++ problem000/m063_unique_paths_ii.go
@@ -0,0 +1,40 @@
+package problem000
+
+/*
+*
+63. 不同路径 II
+一个机器人位于一个m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+
+输入：obstacleGrid = [
+[0,0,0],
+[0,1,0],
+[0,0,0]]
+输出：2
+解释：3x3 网格的正中间有一个障碍物。
+从左上角到右下角一共有 2 条不同的路径：
+1. 向右 -> 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右 -> 向右
+*/
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	//动态规划
+	n, m := len(obstacleGrid), len(obstacleGrid[0])
+	f := make([]int, m)
+	if obstacleGrid[0][0] == 0 {
+		f[0] = 1
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if obstacleGrid[i][j] == 1 {
+				f[j] = 0
+				continue
+			}
+			if j-1 >= 0 && obstacleGrid[i][j-1] == 0 {
+				f[j] += f[j-1]
+			}
+		}
+	}
+	return f[len(f)-1]
+}
