68287e771fff00704b5fa2ce45155a00aef2d5ae
xionglieq <xionglie.qu@gmail.com>
2022-12-29 22:53:55 +0800

    347. 前 K 个高频元素

---
+++ problem300/m347_top_k_frequent_elements.go
@@ -0,0 +1,66 @@
+package problem300
+
+import (
+	"math/rand"
+	"time"
+)
+
+/*
+**
+347. 前 K 个高频元素
+给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+
+示例 1:
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+
+示例 2:
+输入: nums = [1], k = 1
+输出: [1]
+
+提示：
+1 <= nums.length <= 10^5
+k 的取值范围是 [1, 数组中不相同的元素的个数]
+题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+*/
+func topKFrequent(nums []int, k int) []int {
+	occurrences := map[int]int{}
+	for _, num := range nums {
+		occurrences[num]++
+	}
+	values := [][]int{}
+	for key, value := range occurrences {
+		values = append(values, []int{key, value})
+	}
+	ret := make([]int, k)
+	qsort(values, 0, len(values)-1, ret, 0, k)
+	return ret
+}
+
+func qsort(values [][]int, start, end int, ret []int, retIndex, k int) {
+	rand.Seed(time.Now().UnixNano())
+	picked := rand.Int()%(end-start+1) + start
+	values[picked], values[start] = values[start], values[picked]
+
+	pivot := values[start][1]
+	index := start
+
+	for i := start + 1; i <= end; i++ {
+		if values[i][1] >= pivot {
+			values[index+1], values[i] = values[i], values[index+1]
+			index++
+		}
+	}
+	values[start], values[index] = values[index], values[start]
+	if k <= index-start {
+		qsort(values, start, index-1, ret, retIndex, k)
+	} else {
+		for i := start; i <= index; i++ {
+			ret[retIndex] = values[i][0]
+			retIndex++
+		}
+		if k > index-start+1 {
+			qsort(values, index+1, end, ret, retIndex, k-(index-start+1))
+		}
+	}
+}
