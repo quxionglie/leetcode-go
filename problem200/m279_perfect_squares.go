4ceb3bc7a3a8bf431119e56828472d5cb1bcc275
xionglieq <xionglie.qu@gmail.com>
2023-01-03 22:27:55 +0800

    279. 完全平方数

---
+++ problem200/m279_perfect_squares.go
@@ -0,0 +1,51 @@
+package problem200
+
+import "math"
+
+/*
+*
+279. 完全平方数
+给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
+完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+示例  1：
+输入：n = 12
+输出：3
+解释：12 = 4 + 4 + 4
+
+示例 2：
+输入：n = 13
+输出：2
+解释：13 = 4 + 9
+
+提示：
+1 <= n <= 10^4
+*/
+// 判断是否为完全平方数
+func isPerfectSquare(x int) bool {
+	y := int(math.Sqrt(float64(x)))
+	return y*y == x
+}
+
+// 判断是否能表示为 4^k*(8m+7)
+func checkAnswer4(x int) bool {
+	for x%4 == 0 {
+		x /= 4
+	}
+	return x%8 == 7
+}
+
+func numSquares(n int) int {
+	if isPerfectSquare(n) {
+		return 1
+	}
+	if checkAnswer4(n) {
+		return 4
+	}
+	for i := 1; i*i <= n; i++ {
+		j := n - i*i
+		if isPerfectSquare(j) {
+			return 2
+		}
+	}
+	return 3
+}
