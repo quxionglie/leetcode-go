6a8a24dd730f620dccf5ed7628a42390c1204aca
xionglieq <xionglie.qu@gmail.com>
2023-01-29 22:14:12 +0800

    518. 零钱兑换 II

---
+++ problem500/m518_coin_change_ii_test.go
@@ -0,0 +1,39 @@
+package problem500
+
+import "testing"
+
+func Test_change(t *testing.T) {
+	type args struct {
+		amount int
+		coins  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				amount: 5,
+				coins:  []int{1, 2, 5},
+			},
+			want: 4,
+		},
+		{
+			name: "",
+			args: args{
+				amount: 2,
+				coins:  []int{2},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := change(tt.args.amount, tt.args.coins); got != tt.want {
+				t.Errorf("change() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
