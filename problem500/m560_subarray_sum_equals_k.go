f2bb57f189b2829ba90c81d491c6ad2e5d782aaa
xionglieq <xionglie.qu@gmail.com>
2023-02-09 17:23:30 +0800

    560. 和为 K 的子数组

---
+++ problem500/m560_subarray_sum_equals_k.go
@@ -0,0 +1,32 @@
+package problem500
+
+/*
+*
+560. 和为 K 的子数组
+给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+示例 1：
+输入：nums = [1,1,1], k = 2
+输出：2
+
+示例 2：
+输入：nums = [1,2,3], k = 3
+输出：2
+
+提示：
+1 <= nums.length <= 2 * 10^4
+-1000 <= nums[i] <= 1000
+-10^7 <= k <= 10^7
+*/
+func subarraySum(nums []int, k int) int {
+	count := 0
+	for start := 0; start < len(nums); start++ {
+		sum := 0
+		for end := start; end >= 0; end-- {
+			sum += nums[end]
+			if sum == k {
+				count++
+			}
+		}
+	}
+	return count
+}
---
+++ problem500/m560_subarray_sum_equals_k_test.go
@@ -0,0 +1,39 @@
+package problem500
+
+import "testing"
+
+func Test_subarraySum(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 1, 1},
+				k:    2,
+			},
+			want: 2,
+		},
+		{
+			name: "",
+			args: args{
+				nums: []int{1, 2, 3},
+				k:    2,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subarraySum(tt.args.nums, tt.args.k); got != tt.want {
+				t.Errorf("subarraySum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
